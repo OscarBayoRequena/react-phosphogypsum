8df36673c83b30e67cb0a1804fcd7521e5c37a79
OscarBayoRequena <oscarbayo2002@gmail.com>
2025-12-09 13:56:03 +0100

    Navigation bar created and styled

--- src/components/structure/Navigation.jsx
+++ src/components/structure/Navigation.jsx
@@ -1,18 +1,36 @@
 import React from "react";
+import { Link } from "react-router";
 
 import "./../../assets/css/Navigation.css"
 
 function Navigation (){
 
-    
+    const navItems = [
+        {
+            section:"Home",
+            route: "/"
+        },
+        {
+            section:"Sources",
+            route: "/sources"
+        },
+        {
+            section:"Contact",
+            route: "/contact"
+        },
+        {
+            section:"Authors",
+            route: "/authors"
+        }
+    ]
 
     return(
 
-        <div>
-
-            Foo
-
-        </div>
+        <nav>
+            {navItems.map((value,index) =>
+                    <Link className="nav-item" key={`nav-item-${index}`} to={value.route}>{value.section}</Link>
+            )}
+        </nav>
 
     )
 
